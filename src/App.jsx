2ca2cbe9ad65de1f2aad68965da15dadc1c8d689
Daesy Vences <daesydesign@gmail.com>
2021-07-07 10:01:59 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+// import React from 'react';
+
+// const App = () => {
+//     return (
+//         <div>
+//             <h1>Hola del App Component!</h1>
+//         </div>
+//     );
+// };
+
+// export default App;
+
+
+/*import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+ const App = () => {
+    return (
+        <div>
+            <h1 className="bg-primary text-white">Hola del App Component!</h1>
+        </div>
+    );
+};
+
+export default App;*/
+
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+// import Films from './pages/Films';
+// import FilmsDetails from './pages/FilmsDetails';
+// import People from './pages/People';
+// import PeopleDetails from './pages/PeopleDetails';
+
+// const App = () => {
+//     return (
+//         <BrowserRouter>
+//             <Navbar />
+//             <main className="container mt-5">
+//                 <Switch>
+//                     <Route exact path="/">
+//                         <Home />
+//                     </Route>
+//                     <Route exact path="/users">
+//                         <Films />
+//                     </Route>
+//                     <Route exact path="/users/:userid">
+//                         <FilmsDetails />
+//                     </Route>
+//                     <Route exact path="/todos">
+//                         <People />
+//                     </Route>
+//                     <Route exact path="/todos/:todoid">
+//                         <PeopleDetails />
+//                     </Route>
+//                 </Switch>
+//             </main>
+//         </BrowserRouter>
+//     );
+// };
+
+// export default App;
+
+
+
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+ const App = () => {
+    return (
+        <div>
+            <h1 className="bg-primary text-white">Hola del App Component!</h1>
+        </div>
+    );
+};
+
+export default App;
